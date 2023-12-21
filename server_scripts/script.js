0478c42ae3080c6a3ba95a53e6da93c17ddfc910
UT_64bit <83690503+ut-64bit@users.noreply.github.com>
2023-12-21 09:03:40 +0900

    Update script.js

--- server_scripts/script.js
+++ server_scripts/script.js
@@ -4,33 +4,52 @@ global.deleteItem = []
 
 ServerEvents.recipes((event) => {
   // 特定のレシピを削除
-		removeRecipeID.forEach(element => { event.remove({ id: element }) })
+  removeRecipeID.forEach(element => {
+    event.remove({ id: element })
+  })
 
-		// レシピタイプを削除
-		global.deleteRecipeType.forEach(element => { event.remove({ type: element }) })
-  
-		// アイテムを削除
-		global.deleteItem.forEach(element => { event.remove([{ output: element },{ input: element }]) })
+  // レシピタイプを削除
+  global.deleteRecipeType.forEach(element => {
+    event.remove({ type: element })
+  })
 
-		// 置き換えるアイテム
-		for (const [key, value] of Object.entries(global.replaceItem)) {
-			event.replaceInput({ input: key }, key, value)
-			event.replaceOutput({ output: key }, key, value)
-		}
+  // アイテムを削除
+  global.deleteItem.forEach(element => {
+    event.remove([{ output: element },{ input: element }])
+  })
+
+  // 置き換えるアイテム
+  for (const [key, value] of Object.entries(global.replaceItem)) {
+    event.replaceInput({ input: key }, key, value)
+    event.replaceOutput({ output: key }, key, value)
+  }
 })
 
 LootJS.modifiers(event => {
-	event.addBlockLootModifier("#forge:ores").modifyLoot("#forge:raw_materials", item => {
-		const replacement = AlmostUnified.getReplacementForItem(item);
-		if (replacement.isEmpty()) { return item }
-		replacement.setCount(item.getCount());
-		return replacement;
-	})
+  event.addBlockLootModifier("#forge:ores").modifyLoot("#forge:raw_materials", item => {
+    const replacement = AlmostUnified.getReplacementForItem(item);
+    if (replacement.isEmpty()) {
+      return item;
+    }
+    replacement.setCount(item.getCount());
+    return replacement;
+  })
 
-	global.deleteItem.forEach(element => { event.addLootTableModifier(/.*/).removeLoot(element) })
 
-	for (const [key, value] of Object.entries(global.replaceItem)) { event.addLootTableModifier(/.*/).replaceLoot(key, value) }
+  global.deleteItem.forEach(element => { event.addLootTableModifier(/.*/).removeLoot(element) });
 
-	// 草からドロップする藁を植物の繊維に変更
-	event.addBlockLootModifier("tall_grass").replaceLoot("minecraft:grass", "minecraft:tall_grass");
+  for (const [key, value] of Object.entries(global.replaceItem)) {
+    event.addLootTableModifier(/.*/).replaceLoot(key, value) 
+  }
+
+  // 高い草から高い草がドロップするように変更
+  event.addBlockLootModifier("tall_grass").replaceLoot("minecraft:grass", "minecraft:tall_grass");
 })
+
+ItemEvents.entityInteracted("glass_bottle", event => {
+  if (event.target.type != "minecraft:cow") event.exit();
+  event.item.count--
+  event.player.giveInHand(Item.of("farmersdelight:milk_bottle"))
+  event.target.playSound('entity.cow.milk')
+})
+
